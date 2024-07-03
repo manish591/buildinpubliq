59bf2187f84cfba8574ce4c67e26e2e56d4b7c84
Manish Devrani <manishdevrani777@gmail.com>
2024-07-04 02:37:45 +0530

    added user auth form in login page

---
+++ components/UserAuthForm.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import * as React from "react";
+import Image from "next/image";
+import { signIn } from "next-auth/react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  function handleLoginUser() {
+    signIn("github", { callbackUrl: "/dashboard" });
+  }
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      <Button variant="outline" type="button" onClick={handleLoginUser}>
+        <Image src="/github.svg" width={16} height={16} alt="github" className="w-4 h-4" />
+        GitHub
+      </Button>
+    </div>
+  )
+}
