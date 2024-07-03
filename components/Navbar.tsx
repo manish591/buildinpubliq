1634d11c776503feb6be6c300af5a9164d128f6f
Manish Devrani <manishdevrani777@gmail.com>
2024-07-04 02:37:21 +0530

    added navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { Mountain } from "lucide-react";
+
+export default function Navbar() {
+  return (
+    <header className="px-4 lg:px-6 h-14 flex items-center">
+        <Link href="#" className="flex items-center justify-center" prefetch={false}>
+          <Mountain className="h-6 w-6" />
+          <span className="sr-only">Acme Inc</span>
+        </Link>
+        <nav className="ml-auto flex gap-4 sm:gap-6">
+          <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+            Features
+          </Link>
+          <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+            Resources
+          </Link>
+          <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+            Pricing
+          </Link>
+          <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+            Contact
+          </Link>
+        </nav>
+      </header>
+  )
+}
