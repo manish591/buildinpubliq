143daf2aa211906b9009f8c54ff1c01a5d844c3f
Manish Devrani <manishdevrani777@gmail.com>
2024-07-11 00:06:55 +0530

    updated layout to show messages if no data available

--- components/ProjectUpdateDashboard.tsx
+++ components/ProjectUpdateDashboard.tsx
@@ -1,10 +1,3 @@
-import Link from "next/link";
-
-import { timeAgo } from "@/utils/date";
-import { getAllUpdates } from "@/app/actions/updates";
-
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -12,26 +5,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import ProjectUpdatesTable from "./ProjectUpdatesTable";
 
-export default async function ProjectUpdatesDashboard() {
-  const { data } = await getAllUpdates();
-
+export default function ProjectUpdatesDashboard() {
   return (
     <Card>
       <CardHeader className="px-7">
@@ -39,59 +15,7 @@ export default async function ProjectUpdatesDashboard() {
         <CardDescription>View All your updates here</CardDescription>
       </CardHeader>
       <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Title</TableHead>
-              <TableHead className="hidden sm:table-cell">Repo</TableHead>
-              <TableHead className="hidden sm:table-cell">Type</TableHead>
-              <TableHead className="hidden md:table-cell">Created At</TableHead>
-              <TableHead className="text-right sr-only">View Update</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {
-              data.map(item => {
-                return (
-                  <TableRow key={item.id} className="hover:bg-accent">
-                    <TableCell>
-                      <div className="font-medium">{item.tagline}</div>
-                    </TableCell>
-                    <TableCell className="hidden sm:table-cell">
-                      <Link href={item.project.repositoryUrl} className="hover:underline lowercase">{item.project.fullName}</Link>
-                    </TableCell>
-                    <TableCell className="hidden sm:table-cell">
-                      <Badge className="text-xs" variant="secondary">
-                        {item.type}
-                      </Badge>
-                    </TableCell>
-                    <TableCell className="hidden md:table-cell">{timeAgo(item.createdAt.getTime())}</TableCell>
-                    <TableCell className="text-right">
-                      <Dialog>
-                        <DialogTrigger asChild>
-                        <Button variant="outline">View Update</Button>
-                        </DialogTrigger>
-                        <DialogContent className="sm:max-w-[425px]">
-                          <DialogHeader>
-                            <DialogTitle className="pr-4">{item.tagline}</DialogTitle>
-                          </DialogHeader>
-                          <div className="bg-secondary/30 p-4 rounded-md mt-4">
-                            {item.description}
-                          </div>
-                          <DialogFooter className="mt-6">
-                            <Link href={`https://x.com/compose/post?text=${item.description}`} target="_blank">
-                              <Button>Post On Twitter</Button>
-                            </Link>
-                          </DialogFooter>
-                        </DialogContent>
-                      </Dialog>
-                    </TableCell>
-                  </TableRow>
-                )
-              })
-            }
-          </TableBody>
-        </Table>
+        <ProjectUpdatesTable />
       </CardContent>
     </Card>
   )
--- components/ProjectsGrid.tsx
+++ components/ProjectsGrid.tsx
@@ -5,15 +5,26 @@ export async function ProjectsGrid() {
   const projectsData = await getAllProjects();
 
   return (
-    <div className="py-8 gap-8 md:columns-2 lg:columns-3 w-full">
+    <div className="py-8">
       {
-        projectsData.data.map(project => {
-          return (
-            <div key={project.id} className="py-4">
-              <ProjectCard {...project} />
-            </div>
-          )
-        })
+        projectsData.data.length <= 0 ? (
+          <div className="py-48 text-center border rounded-md">
+            <h2 className="text-2xl font-bold">No projects to show</h2>
+            <p className="mt-2 w-[30%] mx-auto">Start by creating a new project by clicking the button above and filling the required details</p>
+          </div>
+        ):(
+          <div className="gap-8 md:columns-2 lg:columns-3 w-full">
+            {
+              projectsData.data.map(project => {
+                return (
+                  <div key={project.id} className="py-4">
+                    <ProjectCard {...project} />
+                  </div>
+                )
+              })
+            }
+          </div>
+        )
       }
     </div>
   )
