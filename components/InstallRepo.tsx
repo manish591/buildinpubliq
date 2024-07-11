19ca27852d99904fb32c9301b66a2e4f17575a3a
Manish Devrani <manishdevrani777@gmail.com>
2024-07-11 10:29:43 +0530

    fixed env not loading in client

--- components/InstallRepo.tsx
+++ components/InstallRepo.tsx
@@ -11,7 +11,7 @@ export default function InstallRepo() {
         variant="outline"
         className="border py-2 px-3 mt-4" 
         onClick={async () => {
-        window.location.href = `https://github.com/apps/${process.env.GITHUB_APP_NAME}/installations/new`;
+        window.location.href = `https://github.com/apps/${process.env.NEXT_PUBLIC_GITHUB_APP_NAME}/installations/new`;
         }}>
           <div className="flex items-center gap-2">
             <Image src="/github.svg" width={16} height={16} alt="github" className="w-4 h-4" />
