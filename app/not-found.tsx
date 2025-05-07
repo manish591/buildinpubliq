e50bdbb20d35eaa6bdd5675ff68e7f473db81a7a
Manish Devrani <manishdevrani777@gmail.com>
2025-05-07 18:21:52 +0530

    updated contrast in not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,4 @@
-import { MoveLeft } from 'lucide-react';
+import { ArrowLeft, MoveLeft } from 'lucide-react';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 
@@ -7,10 +7,10 @@ export default function NotFound() {
     <main className="grid min-h-full place-items-center">
       <div className="text-center">
         <p className="text-base font-semibold">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+        <h1 className="mt-4 text-3xl font-bold tracking-tight sm:text-5xl">
           Page not found
         </h1>
-        <p className="mt-6 text-base leading-7 text-gray-600">
+        <p className="mt-6 text-base leading-7 text-foreground/50">
           Sorry, we couldn't find the page you're looking for.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
@@ -19,7 +19,7 @@ export default function NotFound() {
               variant="outline"
               className="rounded-md px-3.5 py-2.5 text-sm flex items-center gap-3"
             >
-              <MoveLeft />
+              <ArrowLeft className="h-5 w-5 text-gray-500" />
               <span>Back To Home</span>
             </Button>
           </Link>
