3eed5d532961d7708d71a716c9f4cbd4def72c33
Manish Devrani <manishdevrani777@gmail.com>
2025-06-17 22:07:04 +0530

    fixed zod validation rules

--- components/edit-project-form.tsx
+++ components/edit-project-form.tsx
@@ -20,7 +20,7 @@ import { toast } from 'sonner';
 
 const formSchema = z.object({
   title: z.string().min(2).max(50),
-  description: z.string().min(5).max(280),
+  description: z.string().min(10).max(280),
 });
 
 export type TProject = z.infer<typeof formSchema> & {
