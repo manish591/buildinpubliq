bfe4ed6ba32d25627a9cf8e72e7bce2d7558d420
Manish Devrani <manishdevrani777@gmail.com>
2025-06-23 18:35:50 +0530

    fixed channel not connected modal

--- components/create-new-project-update.tsx
+++ components/create-new-project-update.tsx
@@ -32,65 +32,63 @@ export function CreateNewUpdate({
 }>) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <>
-      {!isLinkedinConnected && !isTwitterConnected ? (
-        <div className="py-4 rounded-md text-center">
-          <p className="text-sm w-[80%] mx-auto text-muted-foreground">
-            you have not connected social channels yet. connect channels to
-            create new updates
-          </p>
-          <Link href="/profile">
-            <Button variant="outline" className="border py-2 px-3 mt-4">
-              <div className="flex items-center gap-2">
-                <LinkIcon></LinkIcon>
-                <span>connect channels</span>
-              </div>
-            </Button>
-          </Link>
-        </div>
-      ) : (
-        <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
-          <DialogTrigger>
-            <Button variant="default" className="flex items-center gap-2">
-              <CirclePlus strokeWidth={2} width={16} height={16} />
-              <span>
-                <span className="hidden sm:inline">create </span>new update
-              </span>
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="p-0 max-w-[350px] sm:max-w-[450px] mt-4">
-            <DialogHeader>
-              <div className="flex items-center md:col-start-2 md:col-span-2">
-                <Card className="text-left w-full p-0 border-none shadow-none">
-                  <CardHeader className="pt-10">
-                    <CardTitle>create new update</CardTitle>
-                    <CardDescription>
-                      fill out the form to create a new update.
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent className="p-0">
-                    <ProjectUpdateForm
-                      isLinkedinConnected={isLinkedinConnected}
-                      isTwitterConnected={isTwitterConnected}
-                      defaultProjectUpdateData={{
-                        id: '',
-                        tagline: '',
-                        description: '',
-                        platform: [],
-                        projectId: projectId,
-                        status: Status.DRAFT,
-                        scheduledAt: new Date(new Date().setHours(0, 0, 0, 0)),
-                      }}
-                      onSubmitFunc={createNewUpdate}
-                      closeModal={setIsModalOpen}
-                    />
-                  </CardContent>
-                </Card>
-              </div>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
-      )}
-    </>
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+      <DialogTrigger>
+        <Button variant="default" className="flex items-center gap-2">
+          <CirclePlus strokeWidth={2} width={16} height={16} />
+          <span>
+            <span className="hidden sm:inline">create </span>new update
+          </span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="p-0 max-w-[350px] sm:max-w-[450px] mt-4">
+        <DialogHeader>
+          {!isLinkedinConnected && !isTwitterConnected ? (
+            <div className="py-4 rounded-md text-center">
+              <p className="text-sm w-[80%] mx-auto text-muted-foreground">
+                you have not connected social channels yet. connect channels to
+                create new updates
+              </p>
+              <Link href="/profile">
+                <Button variant="outline" className="border py-2 px-3 mt-4">
+                  <div className="flex items-center gap-2">
+                    <LinkIcon></LinkIcon>
+                    <span>connect channels</span>
+                  </div>
+                </Button>
+              </Link>
+            </div>
+          ) : (
+            <div className="flex items-center md:col-start-2 md:col-span-2">
+              <Card className="text-left w-full p-0 border-none shadow-none">
+                <CardHeader className="pt-10">
+                  <CardTitle>create new update</CardTitle>
+                  <CardDescription>
+                    fill out the form to create a new update.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="p-0">
+                  <ProjectUpdateForm
+                    isLinkedinConnected={isLinkedinConnected}
+                    isTwitterConnected={isTwitterConnected}
+                    defaultProjectUpdateData={{
+                      id: '',
+                      tagline: '',
+                      description: '',
+                      platform: [],
+                      projectId: projectId,
+                      status: Status.DRAFT,
+                      scheduledAt: new Date(new Date().setHours(0, 0, 0, 0)),
+                    }}
+                    onSubmitFunc={createNewUpdate}
+                    closeModal={setIsModalOpen}
+                  />
+                </CardContent>
+              </Card>
+            </div>
+          )}
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
