9f013ef93c1248086c71eeb1daa740e0b7280329
Manish Devrani <manishdevrani777@gmail.com>
2025-09-08 21:29:02 +0530

    updated the link

--- apps/web/components/web/app-logo.tsx
+++ apps/web/components/web/app-logo.tsx
@@ -1,6 +1,8 @@
+import Link from 'next/link';
+
 export function AppLogo() {
   return (
-    <div className="flex items-center gap-2 app-logo">
+    <Link href="/" className="flex items-center gap-2 app-logo">
       <div className="bg-primary w-7 h-7 rounded-sm flex items-center justify-center">
         <span className="mt-[1px] font-bold text-white text-2xl text-shadow-[1.5px_1.5px_0px_rgba(0,0,0,1)]">
           b
@@ -9,6 +11,6 @@ export function AppLogo() {
       <div className="flex items-center justify-center">
         <span className="font-normal text-xl">buildinpubliq</span>
       </div>
-    </div>
+    </Link>
   );
 }
