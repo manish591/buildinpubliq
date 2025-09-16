7a93808f8d071af0c3fc236a564b8c0fce85f35f
Manish Devrani <manishdevrani777@gmail.com>
2025-09-17 01:38:43 +0530

    updated scheam

---
+++ packages/db/prisma/migrations/20250916195408_post_title_removed/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `postTitle` on the `PostOnChannel` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "PostOnChannel" DROP COLUMN "postTitle";
