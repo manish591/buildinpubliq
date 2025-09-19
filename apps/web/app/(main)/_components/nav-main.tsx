df26aa08291cc19e3a89830d080e7379f8fb61c6
Manish Devrani <manishdevrani777@gmail.com>
2025-09-20 04:09:01 +0530

    added link component

--- apps/web/app/(main)/_components/nav-main.tsx
+++ apps/web/app/(main)/_components/nav-main.tsx
@@ -6,6 +6,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from '@/components/ui/sidebar';
+import Link from 'next/link';
 
 const items = [
   {
@@ -28,10 +29,10 @@ export function NavMain() {
         {items.map((item) => (
           <SidebarMenuItem key={item.title}>
             <SidebarMenuButton asChild>
-              <a href={item.url}>
+              <Link href={item.url}>
                 <item.icon />
                 <span>{item.title}</span>
-              </a>
+              </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
         ))}
