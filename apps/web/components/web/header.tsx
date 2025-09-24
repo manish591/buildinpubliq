f97b43b0b428b1a2bcbeca94e7e12fc5a5ab8b47
Manish Devrani <manishdevrani777@gmail.com>
2025-09-24 23:10:26 +0530

    updated header

--- apps/web/components/web/header.tsx
+++ apps/web/components/web/header.tsx
@@ -1,6 +1,5 @@
-import { ArrowRight, Menu } from 'lucide-react';
+import { IconArrowRight, IconMenu3 } from '@tabler/icons-react';
 import Link from 'next/link';
-import { ThemeToggle } from '@/components/general/theme-toggle';
 import {
   Sheet,
   SheetContent,
@@ -14,83 +13,106 @@ import { Button } from '../ui/button';
 
 export function Header() {
   return (
-    <header className="z-10 px-4 sticky top-0 backdrop-blur-lg">
-      <div className="flex items-center h-16 max-w-5xl px-4 mx-auto gap-10">
+    <header className="z-10 sticky top-0 backdrop-blur-lg">
+      <div className="flex items-center h-16 max-w-6xl px-4 mx-auto gap-10">
         <AppLogo>
           <AppLogo.LogoIcon>
             <span className="mt-[1px] font-bold text-white text-2xl text-shadow-[1.5px_1.5px_0px_rgba(0,0,0,1)]">
               b
             </span>
           </AppLogo.LogoIcon>
-          <AppLogo.AppName>
-            <span className="font-normal text-xl">buildinpubliq</span>
+          <AppLogo.AppName className="font-medium text-xl">
+            Buildinpubliq
           </AppLogo.AppName>
         </AppLogo>
         <Sheet>
-          <SheetTrigger className="block sm:hidden">
-            <Menu></Menu>
+          <SheetTrigger className="block sm:hidden ml-auto">
+            <IconMenu3 />
           </SheetTrigger>
           <SheetContent side="bottom">
             <SheetHeader className="text-left">
-              <SheetTitle>buildinpubliq</SheetTitle>
-              <SheetDescription>
-                share your work in the public to build your personal developer
-                portfolio on social media.
+              <SheetTitle>
+                <AppLogo>
+                  <AppLogo.LogoIcon>
+                    <span className="mt-[1px] font-bold text-white text-2xl text-shadow-[1.5px_1.5px_0px_rgba(0,0,0,1)]">
+                      b
+                    </span>
+                  </AppLogo.LogoIcon>
+                  <AppLogo.AppName className="font-medium text-xl">
+                    Buildinpubliq
+                  </AppLogo.AppName>
+                </AppLogo>
+              </SheetTitle>
+              <SheetDescription className="sr-only">
+                Mobile header
               </SheetDescription>
             </SheetHeader>
-            <nav className="ml-auto gap-4 sm:gap-6 items-center flex">
-              <Link
-                href="#how-it-works"
-                className="text-sm font-medium hover:underline underline-offset-4"
-                prefetch={false}
-              >
-                features
+            <nav className="mt-6 gap-2 flex flex-col">
+              <Link href="#features" className="text-base font-medium">
+                Features
+              </Link>
+              <Link href="#integrations" className="text-base font-medium">
+                Integrations
               </Link>
               <Link
-                href="#benefits"
-                className="text-sm font-medium hover:underline underline-offset-4"
-                prefetch={false}
+                href="https://buildinpubliq.blogbee.site"
+                target="_blank"
+                className="text-base font-medium"
               >
-                how it works
+                Blog
               </Link>
-              <Link
-                href="#contact"
-                className="text-sm font-medium hover:underline underline-offset-4"
-                prefetch={false}
-              >
-                blog
-              </Link>
-              <ThemeToggle />
             </nav>
+            <div className="mt-6 flex flex-col gap-2">
+              <Button
+                variant="outline"
+                size="sm"
+                className="cursor-pointer"
+                asChild
+              >
+                <Link href="/auth">Login</Link>
+              </Button>
+              <Button size="sm" className="cursor-pointer" asChild>
+                <Link href="/auth">
+                  Get Started <IconArrowRight />
+                </Link>
+              </Button>
+            </div>
           </SheetContent>
         </Sheet>
-        <nav className="gap-4 sm:gap-6 items-center hidden sm:flex">
+        <nav className="gap-4 sm:gap-8 items-center hidden sm:flex mt-[2px]">
           <Link
-            href="#"
-            className="font-medium hover:underline underline-offset-4"
-            prefetch={false}
+            href="#features"
+            className="font-normal hover:opacity-80 transition-opacity"
           >
-            features
+            Features
           </Link>
           <Link
-            href="#"
-            className="font-medium hover:underline underline-offset-4"
-            prefetch={false}
+            href="#integrations"
+            className="font-normal hover:opacity-80 transition-opacity"
           >
-            how it works
+            Integrations
           </Link>
           <Link
-            href="#"
-            className="font-medium hover:underline underline-offset-4"
-            prefetch={false}
+            href="https://buildinpubliq.blogbee.site"
+            target="_blank"
+            className="font-normal hover:opacity-80 transition-opacity"
           >
-            blog
+            Blog
           </Link>
         </nav>
-        <div className="ml-auto flex items-center gap-4">
-          <Button variant="outline">login</Button>
-          <Button>
-            get started <ArrowRight />
+        <div className="ml-auto hidden sm:flex items-center gap-4">
+          <Button
+            variant="outline"
+            size="sm"
+            className="cursor-pointer"
+            asChild
+          >
+            <Link href="/auth">Login</Link>
+          </Button>
+          <Button size="sm" className="cursor-pointer" asChild>
+            <Link href="/auth">
+              Get Started <IconArrowRight />
+            </Link>
           </Button>
         </div>
       </div>
